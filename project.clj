f38c9d59792c5a95c34bf3ed90128ff911faa341
Andrew Nguyen <andrew-git@na-consulting.net>
2013-10-24 16:46:39 -0700

    Initial creation of repos

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject titan-clj "0.1.0-SNAPSHOT"
+  :description "Simple library for interacting with Titan DB"
+  :url "http://www.github.com/andrew-nguyen/titan-clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 
+                 [com.thinkaurelius.titan/titan-core "1.5.1"]])
---
+++ src/titan_clj/core.clj
@@ -0,0 +1,8 @@
+(ns titan-clj.core
+  (:import [com.thinkaurelius.titan.core TitanFactory]
+           [org.apache.commons.configuration MapConfiguration]))
+
+(defn connect
+  "Connects to a TitanGraph"
+  [params]
+  )
