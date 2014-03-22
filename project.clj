582b976b085c9ccedd0c1ebbc0c9c1c3c332c859
matt <matt@bodydatasystems.com>
2014-03-22 15:15:36 -0700

    whitespace

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
+
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.typed "0.2.14"]
                  [clj-gremlin "2.4.0-ALPHA-3-SNAPSHOT"]
